3465f9f01da769f88898f58a51e6d1da96dcba30
Robson Pereira Martins <151941775+profRobsonPMartins@users.noreply.github.com>
2024-04-25 23:38:38 -0300

    Add files via upload

---
+++ aula-01-JS/src/operadores/script.js
@@ -0,0 +1,77 @@
+// Operadores
+
+console.log("-------------");
+// Aritimeticos
+// Soma
+let x = 10;
+let y = 5;
+let soma = x + y;
+console.log(soma);
+
+console.log("-------------");
+// Subtração
+let a = 50;
+let b = 35;
+let subtracao = a - b;
+console.log(subtracao);
+
+console.log("-------------");
+// divisao
+let c = 75;
+let d = 5;
+let divisao = c / d;
+console.log(divisao);
+
+console.log("-------------");
+// multiplicacao
+let e = 5;
+let f = 3;
+let multiplicacao = 5 * 3;
+
+console.log("-------------");
+// resto da divisao
+let g = 200;
+let h = 5;
+let restoDivisao = g % h;
+console.log(restoDivisao);
+console.log("-------------");
+
+console.log("Operadores logicos");
+console.log("-------------");
+// Operadores lógicos
+let i = true;
+let j = false;
+let comparacao = i === j; // 3 sinais verifica a tipagem
+console.log(comparacao);
+
+// E logico
+let k = a && b;
+console.log(k);
+
+// Ou logico
+let l = a || b;
+console.log(l);
+
+// Não logico 
+let m = !a;
+console.log(m);
+
+
+// Operadores de comparação
+console.log("-------------");
+console.log("Operadores de comparacao");
+
+let idade = 34;
+let altura = 170;
+
+let maiorQueDezessete = idade > 18; // > maior que
+console.log(maiorQueDezessete);
+console.log("-------------");
+
+let igualAltura = altura === 170; // Igual a
+console.log(igualAltura);
+console.log("-------------");
+
+let diferenteDeDez = altura !== 10;
+console.log(diferenteDeDez);
+
---
+++ aula-01-JS/src/tipos-variaveis/script.js
@@ -0,0 +1,38 @@
+// tipo de dados
+
+// String, boolean, object, inteiro
+
+let numero = 10;
+console.log(typeof numero);
+
+console.log("-------------");
+
+let texto = "Olá, turma do 3ºA!";
+console.log(typeof texto);
+
+console.log("-------------");
+
+let booleano = true;
+console.log(typeof booleano);
+
+console.log("-------------");
+
+let array = [1, 2, 3];
+console.log(typeof array);
+
+console.log("-------------");
+
+let objeto = { nome: "aluminio", idade: "30"};
+console.log(typeof objeto);
+
+console.log("-------------");
+
+let nulo = null;
+console.log(typeof nulo);
+
+console.log("-------------");
+
+let indefinido = undefined;
+console.log(typeof undefined);
+
+console.log("-------------");
